074f4cedad0b033c20c6c1ab106c10222679f50a
homeromeirelesjunior <homeromeirelesjunior@gmail.com>
2020-09-24 23:34:06 +0000

    (24/09) Selection Sort com função de comparação.

--- selection-sort2.js
+++ selection-sort2.js
@@ -1,4 +1,11 @@
-function selectionSort(vetor) {
+/*
+    Função de comparação fnComp
+    - Recebe dois valores para comparação
+    - Retorno:
+        - true se o PRIMEIRO valor for MAIOR que o segundo
+        - false caso contrário
+*/
+function selectionSort(vetor, fnComp) {
     let passadas = 0, comparacoes = 0, totalTrocas = 0
 
     // Função que encontra o menor valor em um segmento de vetor (subvetor)
@@ -6,7 +13,8 @@ function selectionSort(vetor) {
     function encontrarMenor(vetor, inicio) {
         let posMenor = inicio
         for(let i = inicio + 1; i < vetor.length; i++) {
-            if(vetor[i] < vetor[posMenor]) posMenor = i
+            //if(vetor[i] < vetor[posMenor]) posMenor = i
+            if(! fnComp(vetor[i], vetor[posMenor])) posMenor = i    // Usado o ! (not) para garantir a execução da função
             comparacoes++
         }
         return posMenor
@@ -22,7 +30,8 @@ function selectionSort(vetor) {
         // Caso o valor encontrado seja menor que o valor atual,
         // procede-se à troca
         comparacoes++
-        if(vetor[posMenor] < vetor[i]) {
+        //if(vetor[posMenor] < vetor[i]) {
+        if(fnComp(vetor[i], vetor[posMenor])) {    // Ordem dos argumentos invetida
             // Permuta de valores usando desestruturação
             [vetor[posMenor], vetor[i]] = [vetor[i], vetor[posMenor]]
             totalTrocas++
@@ -32,20 +41,13 @@ function selectionSort(vetor) {
     console.log({passadas, comparacoes, totalTrocas})
 }
 
-const nums = [56, 78, 44, 23, 99, 14, 60, 37, 6, 82, 31, 65]
-
-/* console.log(encontrarMenor(nums, 3))        // Retornar 8 (posição do valor 6) */
-
-/* console.time('nums')
-selectionSort(nums)
-console.timeEnd('nums')
-console.log(nums) */
-
-let nomes = require('./dados/100-mil-nomes')
-console.time('Teste nomes')
-selectionSort(nomes)
-console.timeEnd('Teste nomes')
+let candidatos = require('./dados/candidatos-2018')
+console.time('Teste candidatos')
+selectionSort(candidatos, (x, y) => x.NM_CANDIDATO > y.NM_CANDIDATO)
+console.timeEnd('Teste candidatos')
 // Medindo a memória utilizada pelo programa
 let memoria = process.memoryUsage().heapUsed / 1024 / 1024
-console.log(nomes)
+console.log(candidatos)
 console.log('Memória utilizada (MB):', memoria)
+
+// selectionSort tenta otimizar as trocas e faz um número menor de comparações, o bubbleSort troca muitas vezes e executa um número maior de comparações.
